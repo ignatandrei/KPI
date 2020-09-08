6a0a4dda4d1e6507d3ef7d67da38364ceb12a2b7
ignatandrei <ignatandrei@yahoo.com>
2020-09-08 10:56:43 +0300

    back

---
+++ src/ToBeDeleted/Client/Generated/Backend/NETCore3.1/TestWebAPI/Controllers/KPI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestWebAPI_BL;
+using TestWEBAPI_DAL;
+
+namespace TestWebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class KPI : ControllerBase
+    {
+        [HttpGet]
+        public async Task< KPI_SR[]> SRValVindutaVsPlan([FromServices] IRepository<SRCkienti,long> clienti, [FromServices] IRepository<CifreAnuale ,long> ca )
+        {
+            var gen = new KPI_Generator(await clienti.GetAll(), await ca.GetAll());
+            var ret = gen.SR_RealizatVsTarget();
+            return ret;
+        }
+    }
+}
---
+++ src/ToBeDeleted/Client/Generated/Backend/NETCore3.1/TestWebAPI_BL/KPI_SR.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestWebAPI_BL
+{
+    public class KPI_SR
+    {
+        public string Name { get; set; }
+        public decimal VindutVsPlan
+        {
+            get
+            {
+                if (ValoarePlanTotala == 0)
+                    return -1;
+                return ValoareTotalaVinduta / ValoarePlanTotala;
+            }
+        }
+        public decimal ValoareTotalaVinduta { get; set; }
+        public decimal ValoarePlanTotala { get; set; }
+
+
+    }
+    public class KPI_Generator
+    {
+        private readonly SRCkienti[] srclienti;
+        private readonly CifreAnuale[] caList;
+
+        public KPI_Generator(SRCkienti[] srclienti, CifreAnuale[] cas)
+        {
+            this.srclienti = srclienti;
+            this.caList = cas;
+        }
+        public KPI_SR[] SR_RealizatVsTarget()
+        {
+            var ret = new Dictionary<string,KPI_SR>();
+            foreach (var sr in srclienti)
+            {
+                var nameSR = sr.sr;
+                var nameClient = sr.client;
+                var ca = caList.First(it => it.name == nameClient);
+                var valoarevinduta = ca.valoarevinduta;
+                var plan = ca.plan;
+                if (!ret.ContainsKey(nameSR))
+                {
+                    ret.Add(nameSR, new KPI_SR());
+                }
+                var res = ret[nameSR];
+                res.Name = nameSR;
+                res.ValoarePlanTotala += plan;
+                res.ValoareTotalaVinduta += valoarevinduta;
+                ret[nameSR] = res;
+
+            }
+
+            return ret.Values.ToArray();
+        }
+
+    }
+
+}
