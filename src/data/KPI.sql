5d13ed047bb615399eee45860ea0b12ccca52da1
ignatandrei <ignatandrei@yahoo.com>
2020-09-26 11:44:33 +0300

    kpi11

--- src/data/KPI.sql
+++ src/data/KPI.sql
@@ -1,19 +1,52 @@
+DROP TABLE [dbo].[KPI11]
+GO
+
+/****** Object:  Table [dbo].[KPI11]    Script Date: 9/26/2020 11:43:40 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[KPI11](
+	[IDClient] [bigint] NOT NULL,
+	[IDAssVA] [bigint] NOT NULL,
+	[IDManager] [bigint] NULL,
+	[IDCounty] [bigint] NOT NULL,
+	[PlanYTD] [money] NULL,
+	[ActualYTD] [money] NULL
+) ON [PRIMARY]
+
+go
 drop procedure createKPI11
+--DROP TABLE KPI11
 go
 CREATE PROCEDURE createKPI11
 AS
 BEGIN
 	SET NOCOUNT ON;
+	
 declare @year int, @month int
 set @year = Year(getdate())
 set @month = MONTH(getdate())
---select c.Month, * from vwACTPL_Ass_Clients c where c.Year =Year and c.Month <@Month
+
+delete from KPI11
+
+INSERT INTO [KPI11]
+           ([IDClient]
+           ,[IDAssVA]
+           ,[IDManager]
+           ,[IDCounty]
+           ,[PlanYTD]
+           ,[ActualYTD])
 select IDClient, c.IDAssVA,m.IDManager, IDCounty,sum([Plan]) as PlanYTD,sum(Actual) as ActualYTD 
-into KPI11
+--sinto KPI11
 from vwACTPL_Ass_Clients c 
 inner join AssVA m on c.IDAssVA = m.IDManager
 where c.Year =Year and c.Month <@Month
 group by IDClient, c.IDAssVA,IDCounty, m.IDManager
+
+
 
 END
 GO
