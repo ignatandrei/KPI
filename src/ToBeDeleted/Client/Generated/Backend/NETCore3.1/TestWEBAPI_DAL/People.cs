29e6c378b113a6ecb6330abcccdd1197d3cd62ad
ignatandrei <ignatandrei@yahoo.com>
2020-10-08 08:07:57 +0300

    managers

---
+++ src/ToBeDeleted/Client/Generated/Backend/NETCore3.1/TestWEBAPI_DAL/People.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestWebAPI_BL;
+
+namespace TestWEBAPI_DAL
+{
+    public partial class DatabaseContext
+    {
+        public async Task<People[]> GetHierarchical()
+        {
+            var managers = await this.dboAssVA.Where(it => it.idmanager == null).ToArrayAsync();
+            var ret = new List<People>(managers.Length);
+
+            foreach (var item in managers)
+            {
+                var p = new People();
+                p.Manager = item;
+                var next = await FindTeam(p);
+                p.Team = next;
+                ret.Add(p);
+            }
+            return ret.ToArray();
+
+        }
+
+        private async Task<People[]> FindTeam(People manager)
+        {
+            var idManager = manager.Manager.idassva;
+            var team = await dboAssVA.Where(it => it.idmanager == idManager).ToArrayAsync();
+            if (team.Length == 0)
+                return new People[0];
+            var ret = new List<People>(team.Length);
+            foreach (var item in team)
+            {
+                var p = new People();
+                p.Manager = item;
+                var next = await FindTeam(p);
+                p.Team = next;
+                ret.Add(p);
+            }
+            return ret.ToArray();
+        }
+    }
+}
---
+++ src/ToBeDeleted/Client/Generated/Backend/NETCore3.1/TestWebAPI_BL/People.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestWebAPI_BL
+{
+    public class People
+    {
+        public dboAssVA Manager { get; set; }
+        public People[] Team { get; set; }
+
+    }
+}
