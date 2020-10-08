92d99072c07d795bd239ac9c84f18c88fa943cf0
ignatandrei <ignatandrei@yahoo.com>
2020-10-08 08:13:46 +0300

    h

---
+++ src/ToBeDeleted/Client/Generated/Backend/NETCore3.1/TestWebAPI/Filters.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWebAPI_BL;
+using TestWEBAPI_DAL;
+
+namespace TestWebAPI
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+
+    public class FiltersController: ControllerBase
+    {
+        private readonly DatabaseContext dc;
+
+        public FiltersController(DatabaseContext dc)
+        {
+            this.dc = dc;
+        }
+        public async Task<People[]> ManagerFilters()
+        {
+            return await dc.GetHierarchical();
+        }
+    }
+}
