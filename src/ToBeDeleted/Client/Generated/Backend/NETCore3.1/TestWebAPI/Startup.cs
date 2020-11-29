694ec7d893438247190d63e56116a99dcbd20aca
ignatandrei <ignatandrei@yahoo.com>
2020-11-29 08:18:38 +0200

    forwarded

--- src/ToBeDeleted/Client/Generated/Backend/NETCore3.1/TestWebAPI/Startup.cs
+++ src/ToBeDeleted/Client/Generated/Backend/NETCore3.1/TestWebAPI/Startup.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -80,15 +81,19 @@ namespace TestWebAPI
 			services.AddTransient<IRepository<dboCounty,Int64>, dboCounty_Repository>();
             
 			services.AddTransient<IRepositoryView<dbotest>, dbotest_Repository>();
-            
-             
-         }
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-			
-			app.UseBlockly();
+            app.UseForwardedHeaders();
+            app.UseBlockly();
             app.UseBlocklyUI(
                 new BlocklyUIOptions()
                 {
